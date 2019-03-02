54229973243c94513d36f62ed24166652ff33f11
avamascuch <avamascuch@gmail.com>
2019-03-02 15:38:20 +0000

    :00

--- warmupDay5.py
+++ warmupDay5.py
@@ -1,1 +1,11 @@
 # File name: warmupDay5.py
+from random import randint
+player = input('rock (r), paper (p), or scissors (s)?')
+player = input('rock (r)')
+print(player, 'vs')
+chosen = randint(1,3)
+print(chosen)
+if chosen == 1: rock
+computer = 'r'
+if chosen == 2: paper
+if chosen == 3: scissors
